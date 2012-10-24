45487b380f69d749c02a45ccde4f438bd9fdd5f2
Greg Joyce <greg@concretecms.com>
2012-10-24 14:25:41 -0700

    version and changelog

--- packages/image_slider/controller.php
+++ packages/image_slider/controller.php
@@ -6,7 +6,7 @@ class ImageSliderPackage extends Package {
 
 	protected $pkgHandle = 'image_slider';
 	protected $appVersionRequired = '5.3.2b1';
-	protected $pkgVersion = '1.1-devel';
+	protected $pkgVersion = '1.1';
 	
 	public function getPackageDescription() {
 		return t("Provides a full-featured image slider.");
